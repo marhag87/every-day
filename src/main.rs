2ca3b9c943b161bd6438d9965465fb611bc81ad5
Martin Hagström <marhag87@gmail.com>
2026-04-18 20:34:23 +0200

    Add year selector

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+mod svg;
+
 use chrono::{Datelike, NaiveDate, Utc};
 use gloo_storage::{LocalStorage, Storage};
 use std::collections::HashMap;
+use std::collections::HashSet;
+use svg::ArrowLeft;
+use svg::ArrowRight;
 use yew::prelude::Html;
 use yew::{classes, html, Component};
 
 enum AppMsg {
+    SetYear(i32),
     ToggleDate(NaiveDate),
 }
 
 struct App {
+    now: NaiveDate,
+    year: i32,
+    years: HashSet<i32>,
     data: HashMap<NaiveDate, bool>,
 }
 
@@ -17,12 +26,24 @@ impl Component for App {
     type Properties = ();
 
     fn create(_ctx: &yew::prelude::Context<Self>) -> Self {
-        let data = LocalStorage::get("dates").unwrap_or_default();
-        Self { data }
+        let data: HashMap<NaiveDate, bool> = LocalStorage::get("dates").unwrap_or_default();
+        let years = data.keys().map(|day| day.year()).collect::<HashSet<i32>>();
+        let now = Utc::now().naive_utc().date();
+        let year = now.year();
+        Self {
+            now,
+            year,
+            years,
+            data,
+        }
     }
 
     fn update(&mut self, _ctx: &yew::prelude::Context<Self>, msg: Self::Message) -> bool {
         match msg {
+            AppMsg::SetYear(year) => {
+                self.year = year;
+                true
+            }
             AppMsg::ToggleDate(date) => {
                 if self.data.get(&date).is_some() {
                     self.data.remove(&date);
@@ -36,15 +57,33 @@ impl Component for App {
     }
 
     fn view(&self, ctx: &yew::prelude::Context<Self>) -> Html {
-        let now = Utc::now().naive_utc();
-        let year = now.year();
+        let last_year = self.year.saturating_sub(1);
+        let next_year = self.year.saturating_add(1);
         html! {
             <div class={classes!("container")}>
                 <table>
                     <thead>
                         <tr>
                             <th colspan=31>
-                                <h2 class={classes!("title")}>{year}</h2>
+                                <div class={classes!("title-bar")}>
+                                    <div class={classes!("year-selector")}>
+                                        if self.years.contains(&last_year) {
+                                            <div onclick={ctx.link().callback(move |_| AppMsg::SetYear(last_year))} class={classes!("clickable")}>
+                                                <ArrowLeft />
+                                            </div>
+                                        }
+                                    </div>
+                                    <div class={classes!("title")}>
+                                        {self.year}
+                                    </div>
+                                    <div class={classes!("year-selector")}>
+                                    if self.years.contains(&self.year.saturating_add(1)) {
+                                        <div onclick={ctx.link().callback(move |_| AppMsg::SetYear(next_year))} class={classes!("clickable")}>
+                                            <ArrowRight />
+                                        </div>
+                                    }
+                                    </div>
+                                </div>
                             </th>
                         </tr>
                     </thead>
@@ -52,15 +91,15 @@ impl Component for App {
                     for month in 1..=12 {
                         <tr>
                             {
-                                (1..=days_in_month(year, month)).map(|day| {
-                                    let date = NaiveDate::from_ymd_opt(year, month, day as u32).unwrap();
-                                    let mut class = classes!("day");
+                                (1..=days_in_month(self.year, month)).map(|day| {
+                                    let date = NaiveDate::from_ymd_opt(self.year, month, day as u32).unwrap();
+                                    let mut class = classes!("day", "clickable");
                                     // Color of circle
                                     if self.data.get(&date).is_some() {
                                         class.push("selected");
                                     }
                                     // Border for today
-                                    if date.ordinal() == now.ordinal() {
+                                    if date.ordinal() == self.now.ordinal() {
                                         class.push("today");
                                     }
                                     html!{
